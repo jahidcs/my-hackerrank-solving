2932d37b8088c70f6093b0c73171f4fc85ad8ec9
jahidcs <csq.jahid@gmail.com>
2023-03-06 20:49:02 +0600

    unsolved

---
+++ python/03_strings/prac18.py
@@ -0,0 +1,15 @@
+def count_substring(string, sub_string):
+    count = 0
+
+    for i in range(len(string)):
+
+
+    return 0
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
